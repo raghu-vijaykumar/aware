dbce8b658d08fa26749380c0356f1b1613c67a2b
Raghu <raghu@example.com>
2026-07-15 20:28:08 +0530

    test: ad_service 66.7% -> 90.5%

--- client/test/services/ad_service_test.dart
+++ client/test/services/ad_service_test.dart
@@ -6,17 +6,17 @@ import 'package:google_mobile_ads/src/ad_instance_manager.dart';
 import 'package:aware/services/ad_service.dart';
 import 'package:aware/services/ad_config.dart';
 
+MethodChannel _channel() => MethodChannel(
+      'plugins.flutter.io/google_mobile_ads',
+      StandardMethodCodec(AdMessageCodec()),
+    );
+
 void main() {
   group('AdService', () {
     setUp(() {
       TestWidgetsFlutterBinding.ensureInitialized();
       AdService.resetForTesting();
     });
-
-    MethodChannel _channel() => MethodChannel(
-          'plugins.flutter.io/google_mobile_ads',
-          StandardMethodCodec(AdMessageCodec()),
-        );
 
     test('initAdMob handles platform error', () async {
       TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
@@ -64,6 +64,45 @@ void main() {
       expect(AdConfig.instance.nativeAdUnitId, AdConfig.test.nativeAdUnitId);
     });
   });
+
+  group('loadNativeAd', () {
+    test('creates and returns NativeAd instance', () async {
+      AdService.resetForTesting();
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        _channel(),
+        _nativeAdHandler,
+      );
+      await AdService.instance.initAdMob();
+
+      final ad = AdService.instance.loadNativeAd(
+        onAdLoaded: (_) {},
+        onAdFailedToLoad: (_, __) {},
+      );
+      expect(ad, isA<NativeAd>());
+      ad.dispose();
+    });
+  });
+
+  group('loadBannerAd', () {
+    test('creates and returns BannerAd instance', () async {
+      AdService.resetForTesting();
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        _channel(),
+        _bannerAdHandler,
+      );
+      await AdService.instance.initAdMob();
+
+      final ad = AdService.instance.loadBannerAd(
+        size: AdSize.banner,
+        onAdLoaded: (_) {},
+        onAdFailedToLoad: (_, __) {},
+      );
+      expect(ad, isA<BannerAd>());
+      ad.dispose();
+    });
+  });
 }
 
 Future<dynamic> _adsMockHandler(MethodCall methodCall) async {
@@ -71,6 +110,24 @@ Future<dynamic> _adsMockHandler(MethodCall methodCall) async {
 }
 
 Future<dynamic> _adsInitializeHandler(MethodCall methodCall) async {
+  switch (methodCall.method) {
+    case 'MobileAds#initialize':
+      return InitializationStatus(<String, AdapterStatus>{});
+    default:
+      return null;
+  }
+}
+
+Future<dynamic> _nativeAdHandler(MethodCall methodCall) async {
+  switch (methodCall.method) {
+    case 'MobileAds#initialize':
+      return InitializationStatus(<String, AdapterStatus>{});
+    default:
+      return null;
+  }
+}
+
+Future<dynamic> _bannerAdHandler(MethodCall methodCall) async {
   switch (methodCall.method) {
     case 'MobileAds#initialize':
       return InitializationStatus(<String, AdapterStatus>{});
